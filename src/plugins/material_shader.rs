79dda363d405aa9032ac348017ff09bc8048a4d9
Clifton King <cliftonk@gmail.com>
2022-09-01 12:43:01 -0500

    multiple spheres and point lights

--- src/plugins/material_shader.rs
+++ src/plugins/material_shader.rs
@@ -34,34 +34,84 @@ impl Material for GlowyMaterial {
     }
 }
 
+/// returns -1.0 to 1.0
+fn rand_f32() -> f32 {
+    rand::random::<f32>() * 2.0 - 1.0
+}
+
+fn rand_vec() -> Vec3 {
+    Vec3::new(rand_f32(), rand_f32(), rand_f32())
+}
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut glow_materials: ResMut<Assets<GlowyMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    let env_texture = asset_server.load("textures/stone_alley_02_1k.hdr");
-    let material = glow_materials.add(GlowyMaterial { env_texture });
+    let spawn_radius: f32 = 10.0;
+    let max_sphere_radius: f32 = 2.0;
 
-    commands.spawn().insert_bundle(MaterialMeshBundle {
-        mesh: meshes.add(Mesh::from(shape::UVSphere {
-            radius: 1.0,
-            ..default()
-        })),
-        material,
-        ..default()
-    });
-
+    // spawn camera
     commands
         .spawn_bundle(Camera3dBundle {
-            transform: Transform::from_xyz(5.0, 2.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(spawn_radius, spawn_radius, spawn_radius)
+                .looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         })
-        .insert(OrbitCamera::default());
+        .insert(OrbitCamera {
+            distance: spawn_radius * 4.0,
+            ..Default::default()
+        });
 
-    // commands.spawn_bundle(PbrBundle {
-    //     mesh: meshes.add(Mesh::from(shape::Plane { size: 100.0 })),
-    //     material: materials.add(Color::rgb(0.1, 0.1, 0.1).into()),
-    //     ..default()
-    // });
+    // spawn plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 1000.0 })),
+        material: materials.add(Color::rgb(0.1, 0.1, 0.1).into()),
+        transform: Transform::from_xyz(0.0, spawn_radius * -0.9, 0.0),
+        ..Default::default()
+    });
+
+    // spawn spheres
+    let env_texture = asset_server.load("textures/stone_alley_02_1k.hdr");
+    let material = glow_materials.add(GlowyMaterial { env_texture });
+    let mut locs: Vec<Vec3> = vec![];
+    while locs.len() < 100 {
+        let radius = rand_f32().abs() * max_sphere_radius;
+        let loc = rand_vec() * spawn_radius;
+        let rotation = Quat::from_rotation_arc(Vec3::ZERO, rand_vec());
+        // check for collisions
+        if locs
+            .clone()
+            .into_iter()
+            .any(|l| loc.distance(l) < radius * 2.0)
+        {
+            continue;
+        }
+        locs.push(loc);
+
+        commands
+            .spawn()
+            .insert_bundle(MaterialMeshBundle {
+                mesh: meshes.add(Mesh::from(shape::UVSphere {
+                    radius,
+                    ..default()
+                })),
+                material: material.clone(),
+                transform: Transform::from_translation(loc).with_rotation(rotation),
+                ..default()
+            })
+            .add_children(|parent| {
+                parent.spawn_bundle(PointLightBundle {
+                    point_light: PointLight {
+                        intensity: 10_000.0,
+                        radius,
+                        color: Color::rgb(0.5, 0.1, 0.0),
+                        ..default()
+                    },
+                    ..default()
+                });
+            });
+    }
 }
